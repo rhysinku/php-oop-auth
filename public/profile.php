d6b3daf48b1e624d7ab5c1be12f90c36ae6ce0b6
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-08-04 20:57:40 +0800

    creating profile page and adding session

---
+++ public/profile.php
@@ -0,0 +1,15 @@
+<?php include('includes/Header.php') ?>
+<?php include('includes/Navigation.php') ?>
+
+
+<?php 
+
+?>
+
+<main>
+    <div class="wrapper">
+
+    </div>
+</main>
+
+<?php include('includes/Footer.php') ?>
