1ff65e4e1cb8b2f55ce1eb65f3ba85bd516ebd51
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-08-04 12:34:09 +0800

    create db class

--- class/Database.class.php
+++ class/Database.class.php
@@ -2,29 +2,31 @@
 
 class Database {
 
-    private $host = DB_HOST;
-    private $user =  DB_USER;
-    private $pass = DB_PASS;
-    private $dbname = DB_NAME;
+    private $host = "localhost";
+    private $user =  "root";
+    private $pass = "";
+    private $dbname = "oop-auth";
     private $dbh;
     private $stmt;
     private $error;
 
-    public function __contruct(){
-        $dsn = 'mysql:host=' . $this->host . '; dbname=' . $this->dbname;
+    public function __construct(){
+        $dns = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
         $options = array(
-            PDO:: ATTR_PERSISTENT => true;
+            PDO::ATTR_PERSISTENT => true,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         );
-
-        try{
-            $this->dbh = new PDO($dsn , $this->user, $this->pass, $options)
-        }catch(PDOexception $e){
-            $this->error = $e->getMessage();
-            echo $this->error;
+        try {
+           $this->dbh = new PDO($dns, $this->user, $this->pass, $options);
+                    
+        } catch (PDOException $e) {
+            //throw $th;
         }
-
-
-        
     }
+
+    public function query($sql){
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+   
+        
 }
