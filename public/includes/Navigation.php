9464ae1ad724da86397ad3a0072f844e30972bef
rhysinku <rhysinvillahermosa16@gmail.com>
2024-08-07 16:35:07 +0800

    if login nav change to profile and logout

--- public/includes/Navigation.php
+++ public/includes/Navigation.php
@@ -1,11 +1,24 @@
+<?php 
+
+$authAccount = new AuthMiddleware();
+$isLoggin = $authAccount->isLogin(); 
+
+?>
 <header>
     <div class="wrapper">
         <div class="header_con wr_con">
             <h1><a href="index.php">OOP-AUTH</a></h1>
             <nav class="header-nav">
                 <ul>
+
+                <?php if($isLoggin) : ?>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                <?php else: ?>
                     <li><a href="register.php">Register</a></li>
                     <li><a href="login.php">Login</a></li>
+                <?php endif ?>
+
                 </ul>
             </nav>
         </div>
